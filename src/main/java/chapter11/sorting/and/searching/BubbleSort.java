32749e963dc6571ab200312f916f3f8d7f013e62
Konovalov Alexander <aleksaander@ya.ru>
2016-11-18 22:35:07 +0300

    add Bubble sort algorithm to chapter 11

---
+++ src/main/java/chapter11/sorting/and/searching/BubbleSort.java
@@ -0,0 +1,27 @@
+package chapter11.sorting.and.searching;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+
+    public static void pubbleSort(int[] array) {
+
+        for (int i = array.length - 1; i > 0; i--) {
+            for (int j = 0; j < i ; j++) {
+                if (array[j] > array[j+1]) {
+                    int tmp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = tmp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] a = new int[] { 2, 5, 4, 3, 10, 7, 9, 6, 8, 1};
+        Arrays.stream(a).forEach(i -> System.out.printf("%d ", i));
+        System.out.println();
+        pubbleSort(a);
+        Arrays.stream(a).forEach(i -> System.out.printf("%d ", i));
+    }
+}
