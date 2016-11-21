30d6272aa7dc6cf195dc66664731628040ff4dee
Konovalov Alexander <aleksaander@ya.ru>
2016-11-21 14:24:45 +0300

    add Binary Search Modifications code

--- src/main/java/chapter11/sorting/and/searching/BinarySearch.java
+++ src/main/java/chapter11/sorting/and/searching/BinarySearch.java
@@ -3,6 +3,44 @@ package chapter11.sorting.and.searching;
 
 public class BinarySearch {
 
+    public static int binarySearchRecursive(int[] a, int x, int low, int high) {
+        if (low > high) return -1;
+
+        int mid = (low + high) / 2;
+        if (a[mid] < x) {
+            binarySearchRecursive(a, x, mid + 1, high);
+        } else if (a[mid] > x) {
+            binarySearchRecursive(a, x, low, mid - 1);
+        } else {
+            return mid;
+        }
+
+        return - 1;
+    }
+
+    public static int binarySearchRecursiveClosest(int[] a, int x, int low, int high) {
+        if (low > high) {
+            if (high < 0) return low;
+            if (low >= a.length) return high;
+            if (x - a[high] < a[low] - x ) {
+                return high;
+            } else {
+                return low;
+            }
+        };
+
+        int mid = (low + high) / 2;
+        if (a[mid] < x) {
+            binarySearchRecursive(a, x, mid + 1, high);
+        } else if (a[mid] > x) {
+            binarySearchRecursive(a, x, low, mid - 1);
+        } else {
+            return mid;
+        }
+
+        return - 1;
+    }
+
     public static int binarySearch(int[] array, int x) {
         int low = 0;
         int high = array.length - 1;
